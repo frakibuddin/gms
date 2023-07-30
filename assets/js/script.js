6a5870f388d306614ea6ba855f8d48b80c1f8376
frakibuddin <frakibuddin360@gmail.com>
2023-07-30 18:48:13 +0600

    upload version two

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,55 @@
 let body = document.body;
 let modeBtn = document.querySelector("#mode-btns");
-var mode = "bg-dark";
+modeBtn.style.cursor = "pointer";
 
 modeBtn.onclick = () => {
   if (body.classList.contains("bg-dark")) {
-    modeBtn.innerHTML = `<i class="fa-regular fa-moon"></i>`;
-    body.classList.remove("bg-dark");
-    localStorage.setItem("mode", "bg-light");
-    body.classList.add("bg-light");
+    body.setAttribute("class", "bg-light");
+    modeBtn.innerHTML = `<i class="fa-regular fa-moon">`;
   } else {
-    body.classList.remove("bg-light");
-    localStorage.setItem("mode", "bg-dark");
-    body.classList.add("bg-dark");
+    body.setAttribute("class", "bg-dark");
     modeBtn.innerHTML = `<i class="fa-regular fa-sun"></i>`;
   }
 };
 
-mode = localStorage.getItem("mode");
-body.classList.add(mode);
+// onscroll animation
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.1)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+window.onscroll = function () {
+  handleScrollAnimation();
+  let pos = window.scrollY;
+};
