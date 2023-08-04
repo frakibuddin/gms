574d2bbeb08fa27876ebf428bb012ebc591702ad
frakibuddin <frakibuddin360@gmail.com>
2023-08-04 18:06:52 +0600

    updated

--- userpanel/assets/js/script.js
+++ userpanel/assets/js/script.js
@@ -5,16 +5,22 @@ let navber = document.querySelector(".navbar");
 modeBtn.style.cursor = "pointer";
 var mode = "bg-dark";
 
+if (body.getAttribute("class")) {
+  navber.classList.add("navbar-dark");
+}
+
 modeBtn.onclick = () => {
-  if (body.classList.contains("bg-dark")) {
+  if (body.getAttribute("class") == "bg-dark") {
     body.setAttribute("class", "bg-light");
     modeBtn.innerHTML = `<i class="fa-regular fa-moon">`;
+    navber.classList.add("navbar-light");
     navber.classList.remove("navbar-dark");
 
     localStorage.setItem("mode", "bg-light");
   } else {
     body.setAttribute("class", "bg-dark");
     modeBtn.innerHTML = `<i class="fa-regular fa-sun"></i>`;
+    navber.classList.remove("navbar-light");
     navber.classList.add("navbar-dark");
 
     localStorage.setItem("mode", "bg-dark");
@@ -33,6 +39,3 @@ window.setTimeout(function () {
       $(this).slideUp();
     });
 }, 4000);
-
-// new DataTable("#example_wrapper");
-//
